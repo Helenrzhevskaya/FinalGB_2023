aea0962c6cc6c7d428769c1648fe43e119ef3e8f
LenaR7 <helenrzhevskaya@mail.ru>
2023-01-16 13:44:52 +0500

    authotest for backend

---
+++ src/test/java/test_backend/AbstractTest2.java
@@ -0,0 +1,43 @@
+package test_backend;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.ResponseSpecification;
+import org.junit.jupiter.api.BeforeAll;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class AbstractTest2 {
+    static Properties prop2 = new Properties();
+    private static InputStream configFile2;
+    protected static ResponseSpecification responseValidSpecification2;
+    protected static ResponseSpecification responseInvalidSpecification2;
+    private static String baseUrl;
+
+    @BeforeAll
+    static void initTest() throws IOException {
+        configFile2 = new FileInputStream("src/test/resources/2.properties");
+        prop2.load(configFile2);
+        baseUrl = prop2.getProperty("base_url");
+
+        responseValidSpecification2 = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .expectStatusLine("HTTP/1.1 200 OK")
+                .build();
+
+        responseInvalidSpecification2 = new ResponseSpecBuilder()
+                .expectStatusCode(401)
+                .expectStatusLine("HTTP/1.1 401 Unauthorized")
+                .build();
+
+
+        RestAssured.responseSpecification = responseValidSpecification2;
+        RestAssured.responseSpecification = responseInvalidSpecification2;
+    }
+    public static String getBaseUrl() {
+        return baseUrl;
+    }
+}
---
+++ src/test/java/test_backend/Authorization.java
@@ -0,0 +1,62 @@
+package test_backend;
+
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.config.EncoderConfig.encoderConfig;
+
+
+public class Authorization extends AbstractTest2 {
+
+    @Test
+    void authorizationPositiveTest() {
+        String id = given()
+                .config(RestAssured.config().encoderConfig(encoderConfig().encodeContentTypeAs("multipart/form-data", ContentType.TEXT)))
+                .multiPart("username", "GB202301271f49")
+                .multiPart("password", "b71e07f1ca")
+                .when()
+                .post(getBaseUrl())
+                .then()
+                .spec(responseValidSpecification2)
+                .extract()
+                .body()
+                .jsonPath()
+                .get("id")
+                .toString();
+        System.out.println("authorization successful. user_id:  " + id);
+    }
+
+    @Test
+    void authorizationNegativeTest() {
+       String error = given()
+                .config(RestAssured.config().encoderConfig(encoderConfig().encodeContentTypeAs("multipart/form-data; boundary=----WebKitFormBoundary2wtr2pG6VB5dSZrg", ContentType.TEXT)))
+                .multiPart("username", "")
+                .multiPart("password", "")
+                .when()
+                .post(getBaseUrl())
+                .then()
+                .spec(responseInvalidSpecification2)
+                .extract()
+                .body()
+                .jsonPath()
+                .get("error")
+                .toString();
+        System.out.println("Invalid_test with error: " + error);
+    }
+
+    @Test
+    void authorizationNegativeTest2() {
+       given()
+                .config(RestAssured.config().encoderConfig(encoderConfig().encodeContentTypeAs("multipart/form-data; boundary=----WebKitFormBoundary2wtr2pG6VB5dSZrg", ContentType.TEXT)))
+                .multiPart("username", "")
+                .when()
+                .post(getBaseUrl())
+                .then()
+                .statusCode(400).statusLine("HTTP/1.1 400 Bad Request");
+
+
+    }
+}
